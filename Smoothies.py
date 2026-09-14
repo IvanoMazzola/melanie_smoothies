b2459354a230d2f18a18e65506013ad59363ecd7
IvanoMazzola <42871394+IvanoMazzola@users.noreply.github.com>
2026-09-14 14:51:24 +0200

    Update Smoothies.py

--- Smoothies.py
+++ Smoothies.py
@@ -22,8 +22,8 @@ st.write("The name is", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe,use_container_width=True)
+#st.stop()
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ", my_dataframe)
 if ingredients_list:
@@ -32,7 +32,13 @@ if ingredients_list:
     ingredients_string =''
     for ingredient in ingredients_list:
         ingredients_string+=ingredient +' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
     #st.write(ingredients_string)
