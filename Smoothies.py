aaf81ef92e203ca8e43430d87a158e85e1a212ca
IvanoMazzola <42871394+IvanoMazzola@users.noreply.github.com>
2026-09-14 11:57:01 +0200

    Update Smoothies.py
    add api request

--- Smoothies.py
+++ Smoothies.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import os
+import requests  
 
 from snowflake.snowpark.functions import col
 
@@ -41,3 +42,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
